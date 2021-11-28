7aba46241ed53a21ec6d87934a84fa99a5a7ff32
Ryo-shiina <93519500+Ryo-shiina@users.noreply.github.com>
2021-11-29 01:17:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -12,24 +12,55 @@ st.write('私はコーヒーマスターです。あなたが好きなコーヒ�
 
 kaori = st.selectbox(
     'あなたが好きなコーヒーの香りは？',
-    list(['――','甘い香り','芳しい香り'])
+    list(['――','甘い香り','芳しい香り','コーヒー飲めないです'])
     )
-sanmi = st.selectbox(
-    'コーヒーに求める酸味はなんですか？',
-    list(['――','爽やか','適度','華やか'])
+if kaori==('甘い香り'):
+    sanmi = st.selectbox(
+        'コーヒーの酸味は好きですか？',
+        list(['――','好き','普通','嫌い'])
     )
-nigami = st.selectbox(
-    'コーヒーの苦味の好みはなんですか？',
-    list(['――','ソフトな苦み','強い苦み'])
-    )
+    if sanmi==('好き'):
+        st.write('グアテマラコーヒーをオススメします')
+    if sanmi==('普通'):
+        nigami = st.selectbox(
+            'コーヒーの苦味の好みは？',
+            list(['――','苦い方が好き','普通','柔らかめな苦味が好き'])
+        )
+        if nigami==('苦い方が好き'):
+            st.write('ベトナムコーヒーをオススメします')
+        if nigami==('普通'):
+            st.write('グアテマラコーヒーをオススメします')
+        if nigami==('柔らかめな苦味が好き'):
+            st.write('ブルーマウンテンをオススメします')
+    if sanmi==('嫌い'):
+        nigami = st.selectbox(
+            'コーヒーの苦味の好みは？',
+            list(['――','苦い方が好き','普通','柔らかめな苦味が好き'])
+        )
+        if nigami==('苦い方が好き'):
+            st.write('ケニアコーヒーをオススメします')
+        if nigami==('普通'):
+            st.write('カロシ・トラジャをオススメします')
+        if nigami==('柔らかめな苦味が好き'):
+            st.write('モカシダモG4をオススメします')
+if kaori==('芳しい香り'):
+    nigami = st.selectbox(
+        'コーヒーの苦味の好みは？'
+        list(['――','苦い方が好き','普通','柔らかめな苦味が好き'])
+        )
+    if nigami==('苦い方が好き'):
+        st.write('ゴールデンマンデリンをオススメします')
+    if nigami==('普通'):
+        st.write('カロシ・トラジャをオススメします')
+    if nigami==('柔らかめな苦味が好き'):
+        st.write('ドミニカバラオナをオススメします')
+if kaori==('コーヒー飲めないです'):
+    if st.button('そんなあなたに'):
+        st.title('Monster Energy')
+        st.write('良いモンエナライフを！！')
+   
 
-st.write('あなたが選択したのは', kaori,'で' ,sanmi ,'な', nigami,'のあるコーヒーです')
 
-st.write('そんなコーヒーを探しているあなたに私がオススメするのは...')
-
-if st.button('結果を表示'):
-    st.title('Monster Energy')
-    st.write('良いモンエナライフを！！')
     
 
     
